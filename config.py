2417ad6b833dcbc91577f8f6b0fa7bb3c3d8b9aa
Thomas <tp.berg@gmail.com>
2019-02-16 10:07:06 -0800

    app --> breadapp

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config(object):
     # prefer secret keys set at the environment level, providing an alternative if that doesn't exist
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    # use the environment's db url; if missing, use this sqlite path
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # use the environment's db url; if missing, use a local sqlite file
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'breadsheet.db')
+
+    # should SQLAlchemy send a notification to the app every time an object changes?
     SQLALCHEMY_TRACK_MODIFICATIONS = False
