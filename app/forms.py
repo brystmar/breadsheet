e085d98bcae64141ab4d2dff2c0eb9030a0a18e1
Thomas <tp.berg@gmail.com>
2019-02-16 20:31:49 -0800

    adding recipes & steps to db works!

--- app/forms.py
+++ app/forms.py
@@ -6,23 +6,28 @@ from app.models import Recipe
 
 class RecipeForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=5, max=64)])
-    author = StringField('Author', validators=[Length(min=0, max=64)])
-    source = StringField('Source', validators=[Length(min=0, max=128)])
-    difficulty = StringField('Difficulty', validators=[Length(min=0, max=6)])
+    author = StringField('Author', validators=[Length(max=64)])
+    source = StringField('Source', validators=[Length(max=128)])
+    difficulty = StringField('Difficulty', validators=[Length(max=1)])
     submit = SubmitField('Add Recipe')
 
     # ensure recipe name is unique
     def validate_recipename(self, name):
         recipe_name = Recipe.query.filter_by(name=name.data).first()
-        if recipe_name is not None:
+        if recipe_name is not None or recipe_name.lower() == name.data.lower():
             raise ValidationError('Recipe name is already in use.  Please enter a unique name.')
 
-    def __repr__(self):  # tells python how to print objects of this class for debugging
+    def __repr__(self):  # tells python how to print objects of this class to the console while debugging
         return '<RecipeForm: {}>'.format(self.name)
 
 
 class StepForm(FlaskForm):
-    recipe_id = IntegerField('recipe_id', validators=[DataRequired(), Length(min=1, max=8)])
-    number = IntegerField('Step Number', validators=[DataRequired(), Length(min=1, max=3)])
-    text = TextAreaField('Directions', validators=[DataRequired(), Length(min=1, max=512)])
+    recipe_id = IntegerField('recipe_id', validators=[DataRequired()])
+    number = IntegerField('Step Number')
+    text = TextAreaField('Directions', validators=[DataRequired(), Length(max=512)])
     then_wait = DecimalField('Then Wait...', places=2)
+    wait_time_range = StringField('Time Range')
+    submit = SubmitField('Add Step')
+
+    def __repr__(self):  # tells python how to print objects of this class to the console while debugging
+        return '<StepForm #{} for recipe_id: {}>'.format(self.number, self.name)
--- app/models.py
+++ app/models.py
@@ -7,21 +7,21 @@ class Recipe(db.Model):
     name = db.Column(db.String(64), index=True, unique=True, nullable=False)
     author = db.Column(db.String(64), index=True)
     source = db.Column(db.String(128), index=True)
-    difficulty = db.Column(db.String(6), index=True)
+    difficulty = db.Column(db.String(1), index=True)
     date_added = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    # steps = db.relationship('Step', backref='recipe', lazy='dynamic')
+    steps = db.relationship('Step', backref='recipe', lazy='dynamic')
 
     def __repr__(self):  # tells python how to print objects of this class to the console while debugging
-        return '<Recipe db model: {}>'.format(self.name)
+        return '<Recipe id: {}, name: {}>'.format(self.id, self.name)
 
 
 class Step(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
     number = db.Column(db.Integer, index=True)
-    text = db.Column(db.LargeBinary, index=True)
-    then_wait = db.Column(db.Integer, index=True)
+    text = db.Column(db.String, index=True)
+    then_wait = db.Column(db.Float, index=True)
     wait_time_range = db.Column(db.String(64))
 
     def __repr__(self):  # tells python how to print objects of this class to the console while debugging
-        return '<Step db model: {}>'.format(str(self.id) + str(self.recipe_id))
+        return '<Step id: {}, recipe_id: {}>'.format(self.id, self.recipe_id)
