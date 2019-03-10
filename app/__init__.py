0e548abfb7f9ff685cfd9a2dc603a504dbbf9874
Thomas <brystmar@users.noreply.github.com>
2019-03-09 23:27:17 -0800

    before making changes for moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # initialization file that defines this package's name as "app"
 from flask import Flask
+from flask_moment import Moment
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,5 +10,6 @@ breadapp = Flask(__name__)
 breadapp.config.from_object(Config)
 db = SQLAlchemy(breadapp)
 migrate = Migrate(breadapp, db)
+moment = Moment(breadapp)
 
 from app import routes, models
