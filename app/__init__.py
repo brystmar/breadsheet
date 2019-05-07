2fc3e0d724cdc1a4b6a7022d7bf8ece609c924c4
Thomas <brystmar@users.noreply.github.com>
2019-05-07 16:43:53 -0700

    moving to GAE standard, renamed to breadapp to app, renamed breadsheet.py to main.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,43 +1,48 @@
-# initialization file that defines the app as breadapp
+# initialization file that defines items within the app
 from config import Config
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging as logging_util
+from os import path
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
 
+basedir = path.abspath(path.dirname(__file__))
+local = '/documents/dev/' in basedir.lower()
+
 
 def create_app(config_class=Config):
     # initialize logging
-    logfile = 'logs/{}.log'.format(__file__)
-    logging_util.basicConfig(filename=logfile, filemode='w', level=logging_util.DEBUG, datefmt='%H:%M:%S',
+    log_dir = 'logs' if local else 'tmp'
+    log_file = '{dir}/syslog.log'.format(dir=log_dir)
+    logging_util.basicConfig(filename=log_file, level=logging_util.DEBUG, datefmt='%H:%M:%S',
                              format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging_util.getLogger(__name__)
-    logger.info("Logging initialized")
+    logger.info("Logging initialized from the /app/__init__.py file")
 
-    breadapp = Flask(__name__)
-    breadapp.config.from_object(config_class)
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-    bootstrap.init_app(breadapp)
-    db.init_app(breadapp)
-    moment.init_app(breadapp)
+    bootstrap.init_app(app)
+    db.init_app(app)
+    moment.init_app(app)
 
     from app.errors import bp as errors_bp
-    breadapp.register_blueprint(errors_bp)
+    app.register_blueprint(errors_bp)
 
     from app.main import bp as main_bp
-    breadapp.register_blueprint(main_bp)
+    app.register_blueprint(main_bp)
 
     from app.convert import bp as convert_bp
-    breadapp.register_blueprint(convert_bp)
+    app.register_blueprint(convert_bp)
 
     # log errors when running in production mode
-    """if not breadapp.debug and not breadapp.testing:
+    """if not app.debug and not app.testing:
         pass
         if not os.path.exists('logs'):
             os.mkdir('logs')
@@ -46,12 +51,12 @@ def create_app(config_class=Config):
             '%(asctime)s %(levelname)s: %(message)s '
             '[in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
-        breadapp.logger.addHandler(file_handler)
+        app.logger.addHandler(file_handler)
 
-        breadapp.logger.setLevel(logging.INFO)
-        breadapp.logger.info('Breadsheet startup')"""
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Breadsheet startup')"""
 
-    return breadapp
+    return app
 
 
 from app import models
--- config.py
+++ config.py
@@ -1,40 +1,48 @@
-import logging
-import os
+import logging as logging_util
+from os import path, environ
 from google.cloud import firestore
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = path.abspath(path.dirname(__file__))
 local = '/documents/dev/' in basedir.lower()
 print("local = {}".format(local))
+print("__file__ = {}".format(__file__))
+
+# initialize logging
+log_dir = 'logs' if local else 'tmp'
+log_file = '{dir}/syslog.log'.format(dir=log_dir)
+logging_util.basicConfig(filename=log_file, level=logging_util.DEBUG, datefmt='%H:%M:%S',
+                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger_config = logging_util.getLogger(__name__)
+logger_config.info("Logging initialized from the /config.py file")
 
 
 class Config(object):
     # local = True
     # another line for later use
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
-    logging.debug("local = {}".format(local))
-    logging.debug("basedir: {}".format(basedir))
+    logger_config.debug("local = {}".format(local))
+    logger_config.debug("basedir: {}".format(basedir))
     print("local = {}".format(local))
     print("basedir: {}".format(basedir))
 
     if local:
         from dotenv import load_dotenv
-        load_dotenv(os.path.join(basedir, '.env'))
+        load_dotenv(path.join(basedir, '.env'))
 
-        logging.debug("Readme file exists? {}".format(os.path.isfile('README.md')))
-        print("Readme file exists? {}".format(os.path.isfile('README.md')))
+        logger_config.debug("Readme file exists? {}".format(path.isfile('README.md')))
+        print("Readme file exists? {}".format(path.isfile('README.md')))
 
-        SECRET_KEY = os.environ.get('SECRET_KEY') or '1mW7@LN0n32L6ntaj0d8jzsXiAW4mkPL7u5l'
-        BUCKET_NAME = os.environ.get('GCP_BUCKET_NAME') or 'local_bucketname'
-        db_user = os.environ.get('GCP_CLOUDSQL_USER')
-        db_pw = os.environ.get('GCP_CLOUDSQL_PW')
-        db_name = os.environ.get('GCP_CLOUDSQL_DBNAME')
-        db_ip = os.environ.get('GCP_CLOUDSQL_IP')
-        db_port = os.environ.get('GCP_CLOUDSQL_PORT')
-        db_instance = os.environ.get('GCP_CLOUDSQL_INSTANCE')
+        SECRET_KEY = environ.get('SECRET_KEY') or '1mW7@LN0n32L6ntaj0d8jzsXiAW4mkPL7u5l'
+        BUCKET_NAME = environ.get('GCP_BUCKET_NAME') or 'local_bucketname'
+        db_user = environ.get('GCP_CLOUDSQL_USER')
+        db_pw = environ.get('GCP_CLOUDSQL_PW')
+        db_name = environ.get('GCP_CLOUDSQL_DBNAME')
+        db_ip = environ.get('GCP_CLOUDSQL_IP')
+        db_port = environ.get('GCP_CLOUDSQL_PORT')
+        db_instance = environ.get('GCP_CLOUDSQL_INSTANCE')
 
     else:
-        logging.debug("JSON file exists? {}".format(os.path.isfile('breadsheet-prod.json')))
-        print("JSON file exists? {}".format(os.path.isfile('breadsheet-prod.json')))
+        logger_config.debug("JSON file exists? {}".format(path.isfile('breadsheet-prod.json')))
+        print("JSON file exists? {}".format(path.isfile('breadsheet-prod.json')))
 
         # supplying the private (prod) key to explicitly use creds for the default service acct
         fire = firestore.Client().from_service_account_json('breadsheet-prod.json')
@@ -60,10 +68,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = db_url
     # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://USER:PW@/breadsheet?host=/cloudsql/breadsheet:us-west1:breadsheet'
 
-    logging.debug("SQLALCHEMY_DATABASE_URI = {}".format(db_url))
+    logger_config.debug("SQLALCHEMY_DATABASE_URI = {}".format(db_url))
     print("SQLALCHEMY_DATABASE_URI = {}".format(db_url))
 
-    logging.debug("BUCKET_NAME = {}".format(BUCKET_NAME))
+    logger_config.debug("BUCKET_NAME = {}".format(BUCKET_NAME))
     print("BUCKET_NAME = {}".format(BUCKET_NAME))
 
     # silence the madness
