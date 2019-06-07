d238b88f228ded9d2e1662554936dc6cb4255704
Thomas <tp.berg@gmail.com>
2019-06-06 17:21:45 -0700

    Homepage works from DynamoDB!

--- global_logger.py
+++ global_logger.py
@@ -10,7 +10,7 @@ if local:
     log_dir = 'logs' if local else 'tmp'
     if not path.exists(log_dir):
         mkdir(log_dir)
-    log_file = '{dir}/syslog.log'.format(dir=log_dir)
+    log_file = f'{log_dir}/syslog.log'
 
     logging.basicConfig(filename=log_file, level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S',
                         format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
@@ -22,6 +22,6 @@ else:
 
 glogger.info("\n\n=======================================")
 glogger.info("Global logging initialized!")
-glogger.info("local = {}".format(local))
+glogger.info(f"local = {local}")
 # print("local = {}".format(local))
 # print("__file__ = {}".format(__file__))
--- main.py
+++ main.py
@@ -36,8 +36,9 @@ if not local:
 
 app = create_app()
 
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8800, debug=True)
+    logger.info("Running locally!")
 
 
 @app.shell_context_processor
