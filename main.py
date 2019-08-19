07ab184966925f4a8482e53c6b70ebb5f44922e6
Thomas <brystmar@users.noreply.github.com>
2019-08-19 15:46:59 -0700

    Redirect logic commented out

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ logger.debug("Just executed app = create_app()")
 def handle_before_request():
     # Redirect breadsheet.appspot.com requests to my domain
     logger.info(f"User agent request: {request.user_agent.string}")
+    logger.info(f"Data type of the above value: {type(request.user_agent.string)}")
     logger.info(f"Request host: {request.host}, lower: {request.host.lower()}")
 
-    if 'AppEngine-Google' not in request.user_agent.string:
-        logger.debug(f"AppEngine-Google not in {request.user_agent.string}!!")
-        if 'breadsheet.appspot.com' in request.host.lower():
-            logger.debug(f"breadsheet.appspot.com in {request.host.lower()}!!")
-            logger.info(f"Requested URL: {request.url}, redirecting to: {Config.domain_url}!!")
-            return redirect(Config.domain_url, code=301)
+    # if 'AppEngine-Google' not in request.user_agent.string:
+    #     logger.debug(f"AppEngine-Google not in {request.user_agent.string}!!")
+    #     if 'breadsheet.appspot.com' in request.host.lower():
+    #         logger.debug(f"breadsheet.appspot.com in {request.host.lower()}!!")
+    #         logger.info(f"Requested URL: {request.url}, redirecting to: {Config.domain_url}!!")
+    #         return redirect(Config.domain_url, code=301)
 
 
 if __name__ == '__main__' and local:
