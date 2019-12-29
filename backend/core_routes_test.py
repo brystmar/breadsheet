0d712d6b16cd07911ab1409787ab1980d6b5bb2d
Thomas <tp.berg+pycharmraktop@gmail.com>
2019-12-29 05:45:08 -0800

    Vacation changes from laptop

--- backend/core_routes_test.py
+++ backend/core_routes_test.py
@@ -1,2 +1,12 @@
+from backend.core_routes import DocumentationApi, ReadmeApi
 import pytest
 
+
+class TestDocumentationApi:
+    def test_get(self):
+        pass
+
+
+class TestReadmeApi:
+    def test_get(self):
+        pass
